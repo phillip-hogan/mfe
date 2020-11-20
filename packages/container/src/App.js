838c953a77dec9e53333515bb4f2735baabe6e47
Phillip Hogan <phillip.hogan@elucidat.com>
2020-11-20 13:39:11 +0000

    Add text to fire action

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi there!</h1>
+      <h1>Hi there! This is the container/host here.</h1>
       <hr />
       <MarketingApp />
     </div>
