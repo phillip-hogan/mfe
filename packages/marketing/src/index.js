85bf832cd00891ecfddfb4507c5ec90817c1ac26
Phillip Hogan <phillip.hogan@elucidat.com>
2020-11-20 12:20:11 +0000

    Init commit

---
+++ packages/marketing/src/index.js
@@ -0,0 +1,5 @@
+import('./bootstrap');
+
+// Use an import "function()" to call in bootstrap file
+// this gives webpack time to load in any shared deps before
+// trying to use them in ./ bootstrap
